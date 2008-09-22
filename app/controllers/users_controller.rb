f2c8cb6d9aed8181ea483269799cd844a6650c4c
Daniel Haran <chebuctonian@mgmail.com>
2008-09-22 08:03:45 -0400

    simplify conditionals on activation_code in user activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,15 +19,16 @@ class UsersController < ApplicationController
 
   def activate
     logout_keeping_session!
-    user = User.find_by_activation_code(params[:activation_code]) unless params[:activation_code].blank?
-    case
-    when params[:activation_code] && user && !user.active?
+    if params[:activation_code].blank?
+      flash[:error] = "The activation code was missing.  Please follow the URL from your email."
+      return redirect_back_or_default(root_url)
+    end
+    
+    user = User.find_by_activation_code(params[:activation_code])
+    if user && !user.active?
       user.activate!
       flash[:notice] = "Signup complete! Please sign in to continue."
       redirect_to  login_url
-    when params[:activation_code].blank?
-      flash[:error] = "The activation code was missing.  Please follow the URL from your email."
-      redirect_back_or_default root_url
     else 
       flash[:error]  = "We couldn't find a user with that activation code -- check your email? Or maybe you've already activated -- try signing in."
       redirect_back_or_default root_url
